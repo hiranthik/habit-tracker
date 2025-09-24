43a77ca9170acb782099be90791f133252c1d372
hiranthik <hiranthikulathunga80@gmail.com>
2025-09-24 11:48:06 -0400

    Welcome page
    Introduces users to the app with a simple and engaging design, providing a clear starting point before account creation or login. Navigation support to sign-up/login flow using the expo router.

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+import {Client, Account, Databases} from 'react-native-appwrite';
+
+const client = new Client().setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!)
+.setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT!)
+.setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM!);
+
+export const account = new Account(client);
